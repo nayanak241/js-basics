0e5741feefea80313d180bbad1bb9fe0d286deb9
nayanak241 <157300482+nayanak241@users.noreply.github.com>
2025-03-31 07:09:25 +0000

    saved data

---
+++ 01_Basics/03_conversionType.js
@@ -0,0 +1,28 @@
+let score = "33ab"
+let scorre = null;
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof  valueInNumber);
+console.log(valueInNumber);//Nan not a number
+console.log(typeof scorre);
+
+//"33" =>33
+//"33abc" => NaN
+//true =>1;
+//false =>0;
+ let isLoggedIn = 1
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn);
+ console.log(booleanIsLoggedIn);
+
+ //""=>false
+ //"nayana"=>true
+
+ let someNumber = 33
+ 
+ let stringNumber = String(someNumber);
+ console.log(stringNumber);
+ console.log( typeof stringNumber);
