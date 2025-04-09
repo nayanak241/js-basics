9869971c1f5ba075aed6cd8a80db860d101cdbf2
nayanak241 <157300482+nayanak241@users.noreply.github.com>
2025-04-09 05:47:39 +0000

    learnt scope function

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -10,3 +10,41 @@ if(true){
 console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "nayana"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+
+}
+
+// one()
+
+if(true){
+    const username = "nayana";
+    if(username === "nayana"){
+        const website = "  youtube"
+        console.log(username + website)
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+//++++++++++++++++ intersting ++++++++++++++++
+
+console.log(addone(5));
+function addone(num){
+    return num +1
+}
+
+
+
+const addTwo = function(num){
+    return num +2;
+}
+addTwo(5)
