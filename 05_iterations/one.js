6fdb03653cda9a54a2b957ccd31f1486c66365b6
nayanak241 <157300482+nayanak241@users.noreply.github.com>
2025-04-19 14:57:47 +0000

    for loop learnt

---
+++ 05_iterations/one.js
@@ -0,0 +1,36 @@
+//for
+// for(let index = 0; index<=10;index++){
+//     const element = index;
+//     if(element == 5){
+//         console.log("5 is a best number");
+//     }
+//     console.log(element);
+// }
+
+// for(let i=0;i<=10;i++){
+//     console.log(`outer loop value:${i}`);
+//     for(let j=0;j<=10;j++){
+//         console.log(`Inner loop value ${j} and inner loop ${i}`);
+//     }
+// }
+
+// let myArray = ["flash","batman","superman"]
+// console.log(myArray.length);
+// for (let index=0; index<myArray.length;index++){
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+//break and continue
+
+for(let index = 1;index<=20;index++){
+    // if(index==5){
+    //     console.log(`Detected 5`);
+    //    continue;//it skip sthat iteration and contnues
+    // }
+    // if(index==5){
+    //     console.log(`Detected 5`);
+    //    break;//after this nothing is printed
+    // }
+    console.log(`value od i is ${index}`);
+}
